a92bc5dfdf8559471cf656c48cee2e3bafb05acb
WhiskyTangoFox <aikimiller@gmail.com>
2022-07-04 14:21:34 +0100

    upload

---
+++ src/main/java/com/test/foobar/IBar.java
@@ -0,0 +1,6 @@
+package com.test.foobar;
+
+public interface IBar {
+
+    public String getSomeOtherData();
+}
---
+++ src/main/java/com/test/foobar/IFoo.java
@@ -0,0 +1,7 @@
+package com.test.foobar;
+
+public interface IFoo {
+
+    IBar getBar();
+
+}
